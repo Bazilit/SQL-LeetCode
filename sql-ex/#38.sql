7ece8d4db453ec5a69cf8edf9e3bb7646fa2ac38
Артем Шарковский <91909975+Bazilit@users.noreply.github.com>
2022-12-02 12:07:33 +0300

    Create #38.sql

---
+++ sql-ex/#38.sql
@@ -0,0 +1,10 @@
+--  Найдите страны, имевшие когда-либо классы обычных боевых кораблей ('bb') и имевшие когда-либо классы крейсеров ('bc').
+
+
+SELECT DISTINCT country
+FROM Classes
+WHERE type = 'bb'
+INTERSECT
+SELECT DISTINCT country
+FROM Classes
+WHERE type = 'bc'
