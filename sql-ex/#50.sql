6da904261bc4447506c7a2dd3dc63c47a8ce0b68
Артем Шарковский <91909975+Bazilit@users.noreply.github.com>
2022-12-06 14:11:18 +0300

    Create #50.sql

---
+++ sql-ex/#50.sql
@@ -0,0 +1,6 @@
+--  Найдите сражения, в которых участвовали корабли класса Kongo из таблицы Ships.
+
+
+SELECT DISTINCT battle
+FROM Outcomes
+WHERE ship IN (SELECT name FROM Ships WHERE class = 'Kongo')
